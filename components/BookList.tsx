9aaec817277dcc60ba4992bcf453ac4541f897a8
Victoria Peart <73324816+v-prt@users.noreply.github.com>
2023-04-23 15:03:29 -0700

    implement pagination for book search

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -4,14 +4,16 @@ import { Book } from '../types'
 
 interface Props {
   books: Book[]
+  infiniteScroll: () => void
 }
 
-export const BookList: React.FC<Props> = ({ books }) => {
+export const BookList: React.FC<Props> = ({ books, infiniteScroll }) => {
   return (
     <FlatList
       data={books}
       keyExtractor={book => book.id}
       renderItem={({ item }) => <BookCard book={item} />}
+      onEndReached={infiniteScroll}
     />
   )
 }
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View, Text } from 'react-native'
-import { useQuery } from 'react-query'
+import { useInfiniteQuery } from 'react-query'
 import { COLORS } from '../GlobalStyles'
 import { BookList } from '../components/BookList'
 import { Input } from '../ui/Input'
@@ -11,32 +11,59 @@ interface Props {}
 
 export const Search: React.FC<Props> = ({}) => {
   const [searchText, setSearchText] = useState<string>('')
-  const [searchResults, setSearchResults] = useState<any[] | null>(null)
+  const [searchResults, setSearchResults] = useState<any>(null)
+  const [totalResults, setTotalResults] = useState<number | null>(null)
 
-  const { data, status } = useQuery(['books', searchText], async () => {
-    if (searchText?.length > 0) {
-      // TODO: pagination using startIndex & maxResults (20), search by genre
-      const res = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${searchText}+inauthor:${searchText}`
-      )
-      return res.data.items
-    } else return null
-  })
+  const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ['books', searchText],
+    async ({ pageParam = 0 }) => {
+      if (searchText?.length > 0) {
+        const maxResults = 20
+        const res = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${searchText}+inauthor:${searchText}&startIndex=${pageParam}&maxResults=${maxResults}`
+        )
+        const items = res.data.items
+        const totalItems = res.data.totalItems
+        return { items, totalItems }
+      } else return { items: [], totalItems: 0 }
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        const maxResults = 20
+        const currentCount = pages.flatMap(page => page?.items).length
+        return currentCount < lastPage?.totalItems ? currentCount + maxResults : undefined
+      },
+    }
+  )
+
+  const filterBooks = (pages: any[]) => {
+    // remove duplicates and books without images
+    const books = pages.flatMap(page => page?.items || [])
+    const uniqueBooks = Array.from(new Set(books.map((book: any) => book.id))).map(id => {
+      const filteredBooks = books.filter(book => book.id === id)
+      const book = filteredBooks[0]
+      return {
+        id: book.id,
+        title: book.volumeInfo.title,
+        image: book.volumeInfo.imageLinks?.thumbnail,
+        author: book.volumeInfo.authors?.[0],
+      }
+    })
+    const booksWithImages = uniqueBooks.filter((book: any) => book.image !== undefined)
+    return booksWithImages
+  }
 
   useEffect(() => {
-    if (data) {
-      setSearchResults(
-        data.map((book: any) => {
-          return {
-            id: book.id,
-            title: book.volumeInfo.title,
-            image: book.volumeInfo.imageLinks?.thumbnail,
-            author: book.volumeInfo.authors?.[0],
-          }
-        })
-      )
+    if (status === 'success' && data) {
+      const searchResults = filterBooks(data.pages)
+      setSearchResults(searchResults)
+      setTotalResults(data.pages[0]?.totalItems)
     }
-  }, [data])
+  }, [status, data])
+
+  const handleLoadMore = () => {
+    fetchNextPage()
+  }
 
   return (
     <View style={styles.screen}>
@@ -45,9 +72,11 @@ export const Search: React.FC<Props> = ({}) => {
           initialValues={{ search: '' }}
           onSubmit={values => {
             setSearchText(values.search)
+            // reset search results
+            setSearchResults(null)
+            setTotalResults(null)
           }}>
           {({ handleChange, handleBlur, handleSubmit, values }) => (
-            // TODO: add cancel/search buttons?
             <Input
               config={{
                 // submit form on change
@@ -61,8 +90,14 @@ export const Search: React.FC<Props> = ({}) => {
           )}
         </Formik>
       </View>
-      {/* TODO: loading state, empty state, error state */}
-      {status === 'success' && searchResults && <BookList books={searchResults} />}
+      {status === 'success' && searchResults?.length > 0 && (
+        <>
+          <Text style={styles.text}>Total Results: {totalResults}</Text>
+          <BookList books={searchResults} infiniteScroll={handleLoadMore} />
+          {isFetchingNextPage && <Text style={styles.text}>Loading...</Text>}
+          {!hasNextPage && <Text style={styles.text}>End of Results</Text>}
+        </>
+      )}
     </View>
   )
 }
@@ -73,7 +108,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   text: {
-    fontSize: 30,
+    fontSize: 12,
+    color: COLORS.accentDark,
   },
   searchBar: {
     margin: 10,
